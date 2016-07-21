4f212c7d5429580a920b4f4a9a27aa53ea6d1d13
Kevin Sidwar <sidwarkd@hardlysoftware.com>
2016-07-20 21:40:15 -0700

    Auto register device on first boot

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ This project started as a simple maker replacement for an Amazon AWS IoT Button.
 ![Hookup diagram for the IoT Button](./hookup.png)
 
 ### Get the Code
-Get your Particle device [set up and connected to your WiFi](https://docs.particle.io/start/). To install the firmware on your Particle device simply copy the contents of [iot-button.ino](./iot-button.ino) into a Particle Build or Particle Dev project. Currently the click behavior is hard-coded but you can modify the `callCloudService()` function to perform any IoT task you want.
+Get your Particle device [set up and connected to your WiFi](https://docs.particle.io/start/). To install the firmware on your Particle device simply copy the contents of [iot-button.ino](./iot-button.ino) into a Particle Build or Particle Dev project along with HttpClient.h and HttpClient.cpp. Currently the click behavior is hard-coded but you can modify the `callCloudService()` function to perform any IoT task you want.
 
 ## How Do I Update It?
 Because the button is meant to be battery powered it takes advantage of the low power modes offered by the Particle firmware. That means that it spends most of its time asleep and therefore can't be updated from the cloud. There is a task to fix this but in the meantime you simply perform the following steps to get your device into an update mode.
@@ -23,9 +23,7 @@ Currently the firmware has only been tested on a Photon but theoretically it sho
 ## Task List
 Currently the project is in its infancy. Here are some enhancements to make it more useful to potential users. If you want to help I'm happy to take PRs.
 
-- [ ] Register device on setup using System.deviceID
-- [ ] Set device cloud job id via a REST call to Particle Cloud
+- [x] Register device on setup using System.deviceID
 - [ ] Create a 3D printed case
 - [ ] Test on a Core and Electron
 - [ ] Check for firmware updates on some interval
-
--- iot-button.ino
+++ iot-button.ino
@@ -1,5 +1,6 @@
 // This #include statement was automatically added by the Particle IDE.
-#include "HttpClient/HttpClient.h"
+#include "HttpClient.h"
+//#define LOGGING
 
 int LED = D7;
 int BUTTON = D2;
@@ -11,23 +12,35 @@ http_response_t response;
 // Headers currently need to be set at init, useful for API keys etc.
 http_header_t headers[] = {
     { "Content-Type", "application/x-www-form-urlencoded" },
+    { "User-Agent", "Photon/0.5.2"},
     { "Accept" , "*/*"},
     { NULL, NULL } // NOTE: Always terminate headers with NULL
 };
 
+// Struct for keeping track of register state and job id
+struct CloudInfo{
+  uint8_t isRegistered;
+  char requestId[33];
+};
+
 void callCloudService(void);
+void registerDevice(void);
 
 void setup() {
+
+    #ifdef LOGGING
+    Serial.begin(9600);
+    #endif
+
     pinMode(LED, OUTPUT);
     pinMode(BUTTON, INPUT);
-    
-    // Set up the request
+
+    // Set up the request here to save processing on wakeup
     request.hostname = "iot-button.appspot.com";
     request.port = 80;
-    request.path = "/jobs/execute";
-    
-    //callCloudService(); // Call the register service here
-    
+
+    registerDevice();
+
     // Go into a low power mode
     System.sleep(BUTTON, RISING);
 }
@@ -40,14 +53,98 @@ void loop() {
     System.sleep(BUTTON, RISING);
 }
 
-void callCloudService(){
-    pinSetFast(LED);
-    delay(500);
-    pinResetFast(LED);
-    
-    // Send the deviceId and requestId
-    request.body = "deviceId=12345&requestId=3be49556e1ff4c0b8c69f0c52e949306&f=json";
+void registerDevice(){
+    int indexOfValue;
+    String requestId;
+    CloudInfo info;
 
-    // Get request
-    http.post(request, response, headers);
+    EEPROM.get(0, info);
+    if(info.isRegistered != 1){
+        #ifdef LOGGING
+        Serial.println("Registering device...");
+        #endif
+
+        // First run, let's register the device
+        request.path = "/register";
+        request.body = String("deviceId=" + System.deviceID() + "&name=iot_button&f=json");
+        http.post(request, response, headers);
+
+        // If registration was successful let's create a hard-coded
+        // job for now
+        if(response.status == 200){
+            #ifdef LOGGING
+            Serial.println("Device successfully registered. Adding job");
+            #endif
+
+            // Create a hard-coded Ubidots job for now
+            request.path = "/jobs/save";
+            request.body = String("deviceId=" + System.deviceID() + "&method=POST&f=json&requestUrl=http://things.ubidots.com/api/v1.6/devices/iot-button?token=ECurkqBlQkpYA3WvpLlBWh6sAcDLnNOJjNkJXkjVgmZHNaNXzkE7JZQt0KdF&headers={\"Content-Type\":\"application/json\"}&payload={\"last_press\":10}");
+            http.post(request, response, headers);
+
+            if(response.status == 200){
+                #ifdef LOGGING
+                Serial.println("Job created successfully.");
+                #endif
+
+                // Parse the job id and update CloudInfo in EEPROM
+                indexOfValue = response.body.indexOf("requestId");
+
+                #ifdef LOGGING
+                Serial.println(response.body);
+                Serial.print("Index of requestId: ");
+                Serial.println(indexOfValue);
+                #endif
+
+                if(indexOfValue != -1){
+                    indexOfValue += 13;
+                    requestId = response.body.substring(indexOfValue, indexOfValue + 32);
+
+                    strcpy(info.requestId,requestId.c_str());
+
+                    #ifdef LOGGING
+                    Serial.println("Parsed requestId: " + requestId);
+                    Serial.print("info.requestId:");
+                    Serial.println(String(info.requestId));
+                    #endif
+
+                    info.isRegistered = 1;
+                    Serial.println("Writing CloudInfo to EEPROM");
+                    EEPROM.put(0, info);
+                }
+            }
+            #ifdef LOGGING
+            else{
+                Serial.println("Job creation failed");
+            }
+            #endif
+        }
+        #ifdef LOGGING
+        else{
+            Serial.println("Register failed: " + String(response.status));
+        }
+        #endif
+    }
+    #ifdef LOGGING
+    else{
+      Serial.println("Device already registered");
+    }
+    #endif
+}
+
+void callCloudService(){
+    CloudInfo info;
+
+    request.path = "/jobs/execute";
+
+    // Send the deviceId and requestId
+    EEPROM.get(0, info);
+    if(info.isRegistered == 1){
+        pinSetFast(LED);
+        request.body = String("deviceId=" + System.deviceID() + "&requestId=" + String(info.requestId) + "&f=json");
+        #ifdef LOGGING
+        Serial.println(request.body);
+        #endif
+        http.post(request, response, headers);
+        pinResetFast(LED);
+    }
 }
